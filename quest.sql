382a408a59b98082fa30b6aa28b6472b59005083
wgjdehh <knightwolf534@gmail.com>
2026-07-04 13:43:26 +0530

    Add files via upload

---
+++ quest.sql
@@ -0,0 +1,86 @@
+-- Walmart Project Queries - MySQL
+
+SELECT * FROM walmart;
+
+
+
+-- Count total records
+SELECT COUNT(*) FROM walmart;
+
+-- Count payment methods and number of transactions by payment method
+SELECT 
+    payment_method,
+    COUNT(*) AS no_payments
+FROM walmart
+GROUP BY payment_method;
+
+-- Count distinct branches
+SELECT COUNT(DISTINCT branch) FROM walmart;
+
+-- Find the minimum quantity sold
+SELECT MIN(quantity) FROM walmart;
+
+-- Business Problem Q1: Find different payment methods, number of transactions, and quantity sold by payment method
+SELECT 
+    payment_method,
+    COUNT(*) AS no_payments,
+    SUM(quantity) AS no_qty_sold
+FROM walmart
+GROUP BY payment_method;
+
+-- Project Question #2: Identify the highest-rated category in each branch
+-- Display the branch, category, and avg rating
+SELECT branch, category, avg_rating
+FROM (
+    SELECT 
+        branch,
+        category,
+        AVG(rating) AS avg_rating,
+        RANK() OVER(PARTITION BY branch ORDER BY AVG(rating) DESC) AS rank
+    FROM walmart
+    GROUP BY branch, category
+) AS ranked
+WHERE rank = 1;
+
+
+
+-- Q: Calculate the total quantity of items sold per payment method
+SELECT 
+    payment_method,
+    SUM(quantity) AS no_qty_sold
+FROM walmart
+GROUP BY payment_method;
+
+-- Q: Determine the average, minimum, and maximum rating of categories for each city
+SELECT 
+    city,
+    category,
+    MIN(rating) AS min_rating,
+    MAX(rating) AS max_rating,
+    AVG(rating) AS avg_rating
+FROM walmart
+GROUP BY city, category;
+
+-- Q: Calculate the total profit for each category
+SELECT 
+    category,
+    SUM(unit_price * quantity * profit_margin) AS total_profit
+FROM walmart
+GROUP BY category
+ORDER BY total_profit DESC;
+
+-- Q: Determine the most common payment method for each branch
+WITH cte AS (
+    SELECT 
+        branch,
+        payment_method,
+        COUNT(*) AS total_trans,
+        RANK() OVER(PARTITION BY branch ORDER BY COUNT(*) DESC) AS rank
+    FROM walmart
+    GROUP BY branch, payment_method
+)
+SELECT branch, payment_method AS preferred_payment_method
+FROM cte
+WHERE rank = 1;
+
+
